0f9673049fb06f2582304ac3044af4cae4df2f29
Thomas WACHE <thomas.wache@intradef.gouv.fr>
2018-12-22 18:07:58 +0100

    Now use {make,get,set,swap}context

--- src/schedule.c
+++ src/schedule.c
@@ -7,90 +7,66 @@
 #include <unistd.h>
 
 #include "check.h"
+#include "log.h"
 #include "schedule.h"
 #include "vector.h"
 
-#define STACK_SIZE 0x400UL
+#define STACK_SIZE 0x23000UL
 #define unreachable()                                                                              \
     do {                                                                                           \
         fprintf(stderr, "%s:%d -> Should not be reached", __FILE__, __LINE__);                     \
         exit(EXIT_FAILURE);                                                                        \
     } while (0)
 
-vector_t *coroutines_env = NULL;
+vector_t *coroutines = NULL;
 struct task {
-    ucontext context;
-    const char *name;
+    ucontext_t context;
+    char *name;
 };
 struct task *current_task = NULL;
+ucontext_t uctx_main;
+size_t new_context_count = 0UL;
 
-void __attribute__((constructor)) __init_scheduler__(void) { vector_init(&coroutines_env); }
+void __attribute__((constructor)) __init_scheduler__(void) { vector_init(&coroutines); }
 
-void __attribute__((destructor)) __exit_scheduler__(void) { vector_free(coroutines_env); }
-
-void start_next_task(void) {
-    jmp_buf cur_env;
-    struct task *task = NULL;
-
-    // Restore a saved task
-    if (vector_pop(coroutines_env, (void **)&task) == false) {
-        longjmp(runtime_return, 1);
-        unreachable();
-    }
-    memcpy(&cur_env, &task->env, sizeof(cur_env));
-    free(task);
-    longjmp(cur_env, 1);
-
-    unreachable();
-}
+void __attribute__((destructor)) __exit_scheduler__(void) { vector_free(coroutines); }
 
 void schedule(void) {
-    jmp_buf cur_env;
-    struct task *task = NULL;
+    struct task *task = current_task;
 
-    // Save the current task
-    if (setjmp(cur_env) == 0) {
-        CHK_NULL(task = malloc(sizeof(cur_env)));
-        memcpy(&task->env, &cur_env, sizeof(cur_env));
-        vector_push(coroutines_env, (void *)task);
-    } else {
-        return;
-    }
-
-    start_next_task();
+    debug("stopping task %s", current_task->name);
+    vector_push(coroutines, current_task);
+    vector_pop(coroutines, (void **)&current_task);
+    debug("restarting task %s", current_task->name);
+    swapcontext(&task->context, &current_task->context);
 }
-
-void task_done(void) { start_next_task(); }
 
 void schedule_task(task_func_t entry, void *arg, const char *name) {
     struct task *task = NULL;
 
     CHK_NULL(task = malloc(sizeof(struct task)));
     CHK_NEG(getcontext(&task->context));
-    task->name = name;
-    CHK_NULL(task->context.uc_stack.ss_sp =
-                 mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1 0));
-    task->context.uc_stack.
-    if (setjmp(cur_env) == 0) {
-        CHK_NULL(task = malloc(sizeof(struct task)));
-        memcpy(&task->env, &cur_env, sizeof(cur_env));
-        vector_push(coroutines_env, (void *)task);
-        printf("Task scheduled for latter\n");
+    if (name != NULL) {
+        task->name = strdup(name);
+        new_context_count++;
     } else {
-        printf("[AFTER ] func = %p / arg = %p\n", context->func, context->args);
-        context->func(context->args);
+        asprintf(&task->name, "task-%lu", ++new_context_count);
     }
+    task->context.uc_stack.ss_size = STACK_SIZE;
+    CHK_NULL(task->context.uc_stack.ss_sp =
+                 mmap(NULL, task->context.uc_stack.ss_size, PROT_READ | PROT_WRITE,
+                      MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
+    task->context.uc_link = &uctx_main;
+    makecontext(&task->context, (void (*)(void))entry, 1, arg);
+    vector_push(coroutines, task);
+    debug("New task created : %s (func=%p, arg=%p, stack=%p)", task->name, entry, arg, task->context.uc_stack.ss_sp);
+
+    return;
 }
 
 void start_runtime(void) {
-    if (vector_len(coroutines_env) == 0) {
-        perror("No task to run");
-        return;
+    while (vector_pop(coroutines, (void **)&current_task)) {
+        debug("starting task %s", current_task->name);
+        swapcontext(&uctx_main, &current_task->context);
     }
-
-    if (setjmp(runtime_return) == 0) {
-        printf("Starting runtime\n");
-        start_next_task();
-    }
-    printf("Ending runtime\n");
 }
--- src/schedule.h
+++ src/schedule.h
@@ -4,7 +4,6 @@
 typedef void (*task_func_t)(void *);
 
 extern void schedule(void);
-extern void task_done(void);
 extern void schedule_task(task_func_t entry, void *arg, const char *name);
 extern void start_runtime(void);
 
